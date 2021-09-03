a01e4ba10aa97b282c561248730ac567a2da9982
AliseBaumane <alise.adamsone@gmail.com>
2021-09-03 19:55:44 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int g = 10000;
+        double kg = g / 1000;
+        System.out.println(kg);
+
+    }
+}
