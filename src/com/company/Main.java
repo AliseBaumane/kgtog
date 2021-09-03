b6c9bf8fb0adcd8094d330685e86a36fdf30320e
AliseBaumane <alise.adamsone@gmail.com>
2021-09-03 20:10:33 +0300

    Added scanner

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,15 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        int g = 10000;
-        double kg = g / 1000;
-        System.out.println(kg);
+       Scanner Scanner = new Scanner(System.in);
+        System.out.println("Enter grams");
+        double grams = Scanner.nextDouble ();
+        double kg = grams / 1000;
+        System.out.println("Kilograms" + kg);
 
     }
 }
